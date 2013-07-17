33e8c62fc9bbf83ff16f674d480e3c8dc57e7b32
chaitanya0bhagvan <chaitanya.bhagavan@gmail.com>
2013-07-17 12:48:07 +0000

    Updated grader.js to read from url

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -37,22 +38,26 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+var buildfn = function(htmlfile, checksfile) {
+    var checkHtmlFile = function(htmlfile, response) {
+        $ = cheerioHtmlFile(htmlfile);
+        var checks = loadChecks(checksfile).sort();
+        var out = {};
+        for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        var outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+    };
+   return checkHtmlFile;
 };
 
 var clone = function(fn) {
@@ -65,12 +70,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL Path to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var htmlContent;
+    var checksfile = program.checks;
+    var checkHtmlFile = buildfn(htmlContent, checksfile);
+    if(program.url !== undefined) {
+        htmlContent = rest.get(program.url).on('complete', checkHtmlFile); 
+    }
+    else if(program.file !== undefined) {
+        var htmlContent = fs.readFileSync(program.file);
+        checkHtmlFile(htmlContent, checksfile);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
 
